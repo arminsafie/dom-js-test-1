51fa04a83aa456237d89f53edc26e561cf174861
arminsafie <arminsafaie92@gmail.com>
2024-01-12 19:01:14 +0330

    drop closing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,21 @@
 //!global vars
 
-const backdropEl = document.getElementById("backdrop");
+const backDropEl = document.getElementById("backdrop");
 const addMovieModal = document.getElementById("add-modal");
 const startAddMovieBtn = document.getElementById("addMovieBtn");
+const btnPassiveInput = document.querySelector(".btn--passive");
+const btnsuccessInput = document.querySelector(".btn--success");
 
 //*showing movie input form
 
-const movieInputHandeler = () => {};
+const toggleBackDrop = () => {
+  backDropEl.classList.toggle("visible");
+};
+
+const movieInputHandeler = () => {
+  addMovieModal.classList.toggle("visible");
+  toggleBackDrop();
+};
+
+startAddMovieBtn.addEventListener("click", movieInputHandeler);
+backDropEl.addEventListener("click", movieInputHandeler);
