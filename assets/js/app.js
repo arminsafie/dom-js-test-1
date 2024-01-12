bb4c7b4224448c69b86ac2b9c0e9aab34729c0ab
arminsafie <arminsafaie92@gmail.com>
2024-01-12 19:08:06 +0330

    clear input

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,17 +5,27 @@ const addMovieModal = document.getElementById("add-modal");
 const startAddMovieBtn = document.getElementById("addMovieBtn");
 const btnPassiveInput = document.querySelector(".btn--passive");
 const btnsuccessInput = document.querySelector(".btn--success");
-
+const userInputAr = document.querySelectorAll("input");
 //*showing movie input form
 
 const toggleBackDrop = () => {
   backDropEl.classList.toggle("visible");
 };
 
+const clearInput = () => {
+  for (const input of userInputAr) {
+    input.value = "";
+  }
+};
+
 const movieInputHandeler = () => {
   addMovieModal.classList.toggle("visible");
   toggleBackDrop();
+  clearInput();
 };
+
+//*eventListeners
 
 startAddMovieBtn.addEventListener("click", movieInputHandeler);
 backDropEl.addEventListener("click", movieInputHandeler);
+btnPassiveInput.addEventListener("click", movieInputHandeler);
