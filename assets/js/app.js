e6bab84cfa501429a72c1a75de06b98cc01c9f31
arminsafie <arminsafaie92@gmail.com>
2024-01-12 18:49:36 +0330

    geting global var

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
+//!global vars
 
+const backdropEl = document.getElementById("backdrop");
+const addMovieModal = document.getElementById("add-modal");
+const startAddMovieBtn = document.getElementById("addMovieBtn");
+
+//*showing movie input form
+
+const movieInputHandeler = () => {};
